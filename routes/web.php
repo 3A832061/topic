0f762a102f75e7d4be79e9e3f7ce9bf1df11fe06
yi xuan <3a832061@gm.student.ncut.edu.tw>
2022-03-30 21:38:22 +0800

    設定路由 - Home、Calendar、Post

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\CalendarController;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[HomeController::class,'index'])->name('index');
 
 Route::middleware([
     'auth:sanctum',
@@ -25,5 +26,25 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+});
+
+Route::prefix('calendar')->group(function(){
+    Route::get('/',[CalendarController::class,'index'])->name('calendars.index');
+    Route::get('{id}',[CalendarController::class,'show'])->name('calendars.show');
+    Route::get('create',[CalendarController::class,'create'])->name('calendars.create');
+    Route::post('/',[CalendarController::class,'store'])->name('calendars.store');
+    Route::get('{id}/edit',[CalendarController::class,'edit'])->name('calendars.edit');
+    Route::post('{id}',[CalendarController::class,'update'])->name('calendars.update');
+    Route::delete('{id}',[CalendarController::class,'delete'])->name('calendars.delete');
+});
+
+Route::prefix('post')->group(function(){
+    Route::get('/',[PostController::class,'index'])->name('posts.index');
+    Route::get('{id}',[PostController::class,'show'])->name('posts.show');
+    Route::get('create',[PostController::class,'create'])->name('posts.create');
+    Route::post('/',[PostController::class,'store'])->name('posts.store');
+    Route::get('{id}/edit',[PostController::class,'edit'])->name('posts.edit');
+    Route::post('{id}',[PostController::class,'update'])->name('posts.update');
+    Route::delete('{id}',[PostController::class,'delete'])->name('posts.delete');
 });
 
