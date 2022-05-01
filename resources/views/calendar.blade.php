cc21a7516d7865d1c5493be6d3b2e35044276678
GUO CHIH-LING <3a832098@gm.student.ncut.edu.tw>
2022-05-01 14:50:55 +0800

    calendar blade置換

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -1,29 +1,18 @@
 @extends('layouts.partials.type')
 @section('title','行事曆')
-
-<main class="flex-shrink-0">
-@include('layouts.nav')
-    <!-- Page Content-->
-    <section class="py-5">
-        <div class="container px-5 my-5">
-            <h3 align="center">行事曆</h3>
-            <iframe src="https://calendar.google.com/calendar/embed?src=nplmeq6n7tjdov4ttr5fo5lajg%40group.calendar.google.com&ctz=Asia%2FTaipei" style="border: 0" width="100%" height="600" frameborder="0" scrolling="no"></iframe>
-        </div>
-</main>
-<!-- Footer-->
-<footer class="bg-dark py-4 mt-auto">
-    <div class="container px-5">
-        <div class="row align-items-center justify-content-between flex-column flex-sm-row">
-            <div class="col-auto"><div class="small m-0 text-white">Copyright &copy; Your Website 2022</div></div>
-            <div class="col-auto">
-                <a class="link-light small" href="#!">Privacy</a>
-                <span class="text-white mx-1">&middot;</span>
-                <a class="link-light small" href="#!">Terms</a>
-                <span class="text-white mx-1">&middot;</span>
-                <a class="link-light small" href="#!">Contact</a>
+@section('index.con')
+    <main class="flex-shrink-0">
+    @include('layouts.nav')
+        <!-- Page Content-->
+        <section class="py-5">
+            <div class="container px-5 my-5">
+                <h3>行事曆</h3>
+                <iframe src="https://calendar.google.com/calendar/embed?src=nplmeq6n7tjdov4ttr5fo5lajg%40group.calendar.google.com&ctz=Asia%2FTaipei" style="border: 0" width="100%" height="600" frameborder="0" scrolling="no"></iframe>
             </div>
-        </div>
-    </div>
-</footer>
+         </section>
+    </main>
+    @include('layouts.footer')
+@endsection
+
 
 
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -36,15 +36,12 @@
             </div>
         </header>
 
-
         <!-- 行事曆-->
         <section class="py-5" id="features">
             <div class="container px-5 my-5">
                 <iframe src="https://calendar.google.com/calendar/embed?src=nplmeq6n7tjdov4ttr5fo5lajg%40group.calendar.google.com&ctz=Asia%2FTaipei" style="border: 0" width="100%" height="600" frameborder="0" scrolling="no"></iframe>
             </div>
         </section>
-
-
 
         <!-- Testimonial section-->
         <div class="py-5 bg-light">
