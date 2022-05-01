e5a06a7262920591e427eca969b16d3aa3ae16f6
GUO CHIH-LING <3a832098@gm.student.ncut.edu.tw>
2022-05-01 18:10:13 +0800

    加入表單2

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,9 +1,42 @@
 @extends('layouts.partials.type')
-@section('title','管樂社')
+@section('form.css')
+    <link href="{{asset('css/clean-blog.min.css')}}" rel="stylesheet">
+
+    <style>
+        .row
+        {padding-left:50px !important;}
+        .mt-4
+        {padding-left: 35px;}
+        /*--*/
+        .form-control-itemname
+        {
+            display: inline;
+            width: 60%;
+            height: 34px;
+            padding: 6px 12px;
+            font-size: 14px;
+            line-height: 1.42857143;
+            color: #555;
+            background-color: #fff;
+            background-image: none;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+        }
+        .form-group
+        {
+            margin-bottom: 15px !important;
+        }
+        #layoutSidenav_content
+        {
+            margin-left:200px !important;
+            margin-bottom:100px !important;
+        }
+    </style>
+@endsection
 @section('index.con')
-    <main class="flex-shrink-0">
     @include('layouts.nav')
     <!-- 公告-->
+    <main class="flex-shrink-0">
         <header class="bg-dark py-5">
             <div class="container px-5">
                 <div class="row gx-5 align-items-center justify-content-center">
@@ -36,127 +69,63 @@
             </div>
         </header>
 
-        <!-- 行事曆-->
-        <section class="py-5" id="features">
-            <div class="container px-5 my-5">
-                <iframe src="https://calendar.google.com/calendar/embed?src=nplmeq6n7tjdov4ttr5fo5lajg%40group.calendar.google.com&ctz=Asia%2FTaipei" style="border: 0" width="100%" height="600" frameborder="0" scrolling="no"></iframe>
-            </div>
-        </section>
+        <div id="layoutSidenav_content">
+            <main>
+                <div class="container-fluid px-4">
+                    <h1 class="mt-4" id="customerz1">表單</h1>
+                </div>
+                <!-- /.row -->
+                <p>
+                <div class="row">
+                    <div class="col-lg-8">
+                        <form action="" method="POST" role="form">
+                            @csrf
+                            <div class="form-group">
+                                <label for="name" class="inline">曲目名稱：</label>
+                                <input name="name" class="form-control-itemname" placeholder="請輸入名稱" value="">
+                            </div>
 
-        <!-- Testimonial section-->
-        <div class="py-5 bg-light">
-            <div class="container px-5 my-5">
-                <div class="row gx-5 justify-content-center">
-                    <div class="col-lg-10 col-xl-7">
-                        <div class="text-center">
-                            <div class="fs-4 mb-4 fst-italic">"Working with Start Bootstrap templates has saved me tons of development time when building new projects! Starting with a Bootstrap template just makes things easier!"</div>
-                            <div class="d-flex align-items-center justify-content-center">
-                                <img class="rounded-circle me-3" src="https://dummyimage.com/40x40/ced4da/6c757d" alt="..." />
-                                <div class="fw-bold">
-                                    Tom Ato
-                                    <span class="fw-bold text-primary mx-1">/</span>
-                                    CEO, Pomodoro
-                                </div>
+                            <div class="form-group">
+                                <label for="name" class="inline">曲目名稱：</label>
+                                <input name="name" class="form-control-itemname" placeholder="請輸入名稱" value="">
                             </div>
-                        </div>
+
+                            <div class="form-group">
+                                <label for="frag" class="inline">聲部：</label>
+                                <select name="part" class="form-control">
+                                    <option value="長笛" selected>長笛</option>
+                                    <option value="豎笛" >豎笛</option>
+                                    <option value="薩克" >薩克</option>
+                                    <option value="法國號" >法國號</option>
+                                    <option value="長號" >長號</option>
+                                    <option value="小號" >小號</option>
+                                    <option value="(上)低音號" >(上)低音號</option>
+                                    <option value="打擊" >打擊</option>
+                                </select>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="page" class="inline">部數：</label>
+                                <input name="page" type="number" class="form-control-itemname" placeholder="請輸入部數（例：Alto Part2）" value="">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="quan" class="inline">份數：</label>
+                                <input name="quan" type="number" class="form-control-itemname" placeholder="份數" value="">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="remark" class="inline">備註：</label>
+                                <input name="remark" class="form-control-itemname" placeholder="" value="">
+                            </div>
+                            <div class="text-right">
+                                <button type="submit" class="btn btn-primary">提交</button>
+                            </div>
+                        </form>
                     </div>
                 </div>
-            </div>
+            </main>
         </div>
-        <!-- Blog preview section-->
-        <section class="py-5">
-            <div class="container px-5 my-5">
-                <div class="row gx-5 justify-content-center">
-                    <div class="col-lg-8 col-xl-6">
-                        <div class="text-center">
-                            <h2 class="fw-bolder">From our blog</h2>
-                            <p class="lead fw-normal text-muted mb-5">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque fugit ratione dicta mollitia. Officiis ad.</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="row gx-5">
-                    <div class="col-lg-4 mb-5">
-                        <div class="card h-100 shadow border-0">
-                            <img class="card-img-top" src="https://dummyimage.com/600x350/ced4da/6c757d" alt="..." />
-                            <div class="card-body p-4">
-                                <div class="badge bg-primary bg-gradient rounded-pill mb-2">News</div>
-                                <a class="text-decoration-none link-dark stretched-link" href="#!"><h5 class="card-title mb-3">Blog post title</h5></a>
-                                <p class="card-text mb-0">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            </div>
-                            <div class="card-footer p-4 pt-0 bg-transparent border-top-0">
-                                <div class="d-flex align-items-end justify-content-between">
-                                    <div class="d-flex align-items-center">
-                                        <img class="rounded-circle me-3" src="https://dummyimage.com/40x40/ced4da/6c757d" alt="..." />
-                                        <div class="small">
-                                            <div class="fw-bold">Kelly Rowan</div>
-                                            <div class="text-muted">March 12, 2022 &middot; 6 min read</div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 mb-5">
-                        <div class="card h-100 shadow border-0">
-                            <img class="card-img-top" src="https://dummyimage.com/600x350/adb5bd/495057" alt="..." />
-                            <div class="card-body p-4">
-                                <div class="badge bg-primary bg-gradient rounded-pill mb-2">Media</div>
-                                <a class="text-decoration-none link-dark stretched-link" href="#!"><h5 class="card-title mb-3">Another blog post title</h5></a>
-                                <p class="card-text mb-0">This text is a bit longer to illustrate the adaptive height of each card. Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            </div>
-                            <div class="card-footer p-4 pt-0 bg-transparent border-top-0">
-                                <div class="d-flex align-items-end justify-content-between">
-                                    <div class="d-flex align-items-center">
-                                        <img class="rounded-circle me-3" src="https://dummyimage.com/40x40/ced4da/6c757d" alt="..." />
-                                        <div class="small">
-                                            <div class="fw-bold">Josiah Barclay</div>
-                                            <div class="text-muted">March 23, 2022 &middot; 4 min read</div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 mb-5">
-                        <div class="card h-100 shadow border-0">
-                            <img class="card-img-top" src="https://dummyimage.com/600x350/6c757d/343a40" alt="..." />
-                            <div class="card-body p-4">
-                                <div class="badge bg-primary bg-gradient rounded-pill mb-2">News</div>
-                                <a class="text-decoration-none link-dark stretched-link" href="#!"><h5 class="card-title mb-3">The last blog post title is a little bit longer than the others</h5></a>
-                                <p class="card-text mb-0">Some more quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            </div>
-                            <div class="card-footer p-4 pt-0 bg-transparent border-top-0">
-                                <div class="d-flex align-items-end justify-content-between">
-                                    <div class="d-flex align-items-center">
-                                        <img class="rounded-circle me-3" src="https://dummyimage.com/40x40/ced4da/6c757d" alt="..." />
-                                        <div class="small">
-                                            <div class="fw-bold">Evelyn Martinez</div>
-                                            <div class="text-muted">April 2, 2022 &middot; 10 min read</div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!-- Call to action-->
-                <aside class="bg-primary bg-gradient rounded-3 p-4 p-sm-5 mt-5">
-                    <div class="d-flex align-items-center justify-content-between flex-column flex-xl-row text-center text-xl-start">
-                        <div class="mb-4 mb-xl-0">
-                            <div class="fs-3 fw-bold text-white">New products, delivered to you.</div>
-                            <div class="text-white-50">Sign up for our newsletter for the latest updates.</div>
-                        </div>
-                        <div class="ms-xl-4">
-                            <div class="input-group mb-2">
-                                <input class="form-control" type="text" placeholder="Email address..." aria-label="Email address..." aria-describedby="button-newsletter" />
-                                <button class="btn btn-outline-light" id="button-newsletter" type="button">Sign up</button>
-                            </div>
-                            <div class="small text-white-50">We care about privacy, and will never share your data.</div>
-                        </div>
-                    </div>
-                </aside>
-            </div>
-        </section>
     </main>
     @include('layouts.footer')
 @endsection
