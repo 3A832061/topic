ae96c0c984a0b685687771a90a05078c1d85c272
GUO CHIH-LING <3a832098@gm.student.ncut.edu.tw>
2022-05-02 16:42:41 +0800

    js修正

--- resources/views/layouts/partials/type.blade.php
+++ resources/views/layouts/partials/type.blade.php
@@ -19,10 +19,9 @@
         @yield('index.con')
 
     <!--js-->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="{{asset('js/scripts.js')}}"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="{{asset('js/scripts.js')}}"></script>
 
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
         <script src="{{asset('js/mana.scripts.js')}}"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous"></script>
@@ -30,6 +29,6 @@
         <script src="assets/demo/chart-bar-demo.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" crossorigin="anonymous"></script>
         <script src="{{asset('js/mana.datatables-simple-demo.js')}}"></script>
-    <!--js-->
+        <!--js-->
     </body>
 </html>
