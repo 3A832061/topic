339f2b46cc6cf41dd5482509e178114df656fa55
yi xuan <3a832061@gm.student.ncut.edu.tw>
2022-06-06 10:20:34 +0800

    欄位命名修正

--- database/migrations/2022_04_09_165459_create_sheet__music_table.php
+++ database/migrations/2022_04_09_165459_create_sheet__music_table.php
@@ -26,7 +26,7 @@ class CreateSheetMusicTable extends Migration
             $table->integer('year')->nullable();;
             $table->integer('price')->nullable();
             $table->boolean('change')->default(1);
-            $table->boolean('check');
+            $table->boolean('check1');
             $table->string('checkyear')->nullable();
             $table->string('remark')->nullable();
             $table->string('pin');
