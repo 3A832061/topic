f1b2ecd585eeaada342366d19fe6169c7fc37a98
yi xuan <3a832061@gm.student.ncut.edu.tw>
2022-06-06 11:48:47 +0800

    修正文字

--- resources/views/information/recruit/createform.blade.php
+++ resources/views/information/recruit/createform.blade.php
@@ -48,7 +48,7 @@
                         <form action="{{route('recruit.store')}}" method="POST" role="form">
                             @csrf
                             <div class="form-group">
-                                <label for="content" class="inline">內容：（換行要打\n）</label>
+                                <label for="content" class="inline">內容：</label>
                                 <textarea id="content" name="content" class="form-control" rows="10" style="overflow: auto;overflow-y: auto; white-space: pre;"></textarea>
                             </div>
 
